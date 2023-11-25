a0783a8da7b9699ea56b88bf028172e158defec9
Fernando Mashimo <fernando.mashimo@yahoo.com.br>
2023-11-25 10:24:48 -0300

    chore: enabling swagger to generate doc for application

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('User Service')
+    .setDescription('User API')
+    .setVersion('1.0')
+    .addTag('user')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
