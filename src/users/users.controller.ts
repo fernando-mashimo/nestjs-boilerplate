99e4a41065e80df86763285be136f648ce459ad2
Fernando Mashimo <fernando.mashimo@yahoo.com.br>
2023-11-25 14:24:46 -0300

    feat: added info for documentation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,14 +10,20 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiCreatedResponse } from '@nestjs/swagger';
 import UserModel from './model/user.model';
+// import UserModel from './model/user.model';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiCreatedResponse({
+    description: 'User has been successfully created.',
+    type: UserModel,
+  })
   @Post()
-  create(@Body() createUserDto: CreateUserDto): Promise<UserModel> {
+  create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
