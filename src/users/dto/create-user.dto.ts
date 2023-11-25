dc593136e23edb5818efe990cd4811b2ce4c7b79
Fernando Mashimo <fernando.mashimo@yahoo.com.br>
2023-11-25 14:14:56 -0300

    feat: created DTO validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsEmail } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  userName: string;
+
+  @IsNotEmpty()
+  password: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  @Transform(({ value }) => value.toLowerCase())
+  email: string;
+
+  @IsNotEmpty()
+  fullName: string;
+
+  status: string;
+}
