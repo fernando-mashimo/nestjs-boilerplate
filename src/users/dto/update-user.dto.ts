2fc3098335ad96c81e45422fd2c4c57df75ded0d
Fernando Mashimo <fernando.mashimo@yahoo.com.br>
2023-11-26 14:18:46 -0300

    feat: defined DTO for user data update

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,17 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { IsEmail } from 'class-validator';
+import { Transform } from 'class-transformer';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  userName: string;
+  password: string;
+
+  @IsEmail()
+  @Transform(({ value }) => value.toLowerCase())
+  email: string;
+
+  fullName: string;
+
+  status: string;
+}
