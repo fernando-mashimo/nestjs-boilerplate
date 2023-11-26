ee094f9c0d880aca3737ace6b0048325d79ddeeb
Fernando Mashimo <fernando.mashimo@yahoo.com.br>
2023-11-26 13:48:59 -0300

    refactor: commented unused exception handler classes

--- src/users/exceptions/exceptions.ts
+++ src/users/exceptions/exceptions.ts
@@ -1,11 +1,17 @@
-interface IException {
+export interface IException {
   message: string;
   error: string;
   statusCode: number;
 }
 
-export class ConflictException implements IException {
-  message: 'User already exists';
-  statusCode: 409;
-  error: 'Conflict';
-}
+// export class ConflictException implements IException {
+//   message: 'User already exists';
+//   statusCode: 409;
+//   error: 'Conflict';
+// }
+
+// export class InternalErrorException implements IException {
+//   message: 'Some error occurred while processing your request';
+//   statusCode: 500;
+//   error: 'Internal Server Error';
+// }
